2062e62d7973b1e09c110625763f5a26d2579dd1
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2022-12-29 19:41:55 +0530

    Update g5.cpp

--- g5.cpp
+++ g5.cpp
@@ -418,6 +418,30 @@ int main(){
 }
 
 
+// (*Q) REVERSE AN ARRAY USING RECURSION>>>>>>
+
+void f(int a[],int i,int n){
+    if(i>n/2)
+        return;
+    swap(a[i],a[n-i-1]);
+    f(a,i+1,n);
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for (int i = 0; i <n; ++i)
+    
+        cin>>a[i];
+    f(a,0,n);
+    for (int i = 0; i <n; ++i)
+    {
+        cout<<a[i]<<" ";
+    }
+
+
+
 // const int m=1e9+7;
 //   void solve(){
 //     int a,b;
