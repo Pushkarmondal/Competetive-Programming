677b7db05189b8f0407f5df3332dad8c08e9a886
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2023-01-07 12:35:33 +0530

    Update g5.cpp

--- g5.cpp
+++ g5.cpp
@@ -474,6 +474,30 @@ int main(){
 cout<<f(4);
 }
 
+
+(*Q)SUBSEQUENCE USING RESURSION >>>>>
+
+void printf(int ind,vector<int> &ds,int a[],int n){
+    if(ind==n){
+        for(auto it: ds){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ds.push_back(a[ind]);
+    printf(ind+1,ds,a,n);
+    ds.pop_back();
+    printf(ind+1,ds,a,n);
+}
+
+int main(){
+    int a[]={3,1,2};
+    int n=3;
+    vector<int> ds;
+    printf(0,ds,a,n);
+}
+
 // const int m=1e9+7;
 //   void solve(){
 //     int a,b;
