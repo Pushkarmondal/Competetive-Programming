cefcfd8edaaeeb3723a64368e5b428acc0aed306
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2023-01-16 16:46:41 +0530

    Update g5.cpp

--- g5.cpp
+++ g5.cpp
@@ -541,6 +541,25 @@ int main(){
 
 
 
+(*Q) SUM OF ALL ELEMENTS OF AN ARRAY >>>>>>
+
+int f(int *arr,int idx,int n){
+    if(idx==n-1){
+        return  arr[idx];
+    }
+    return arr[idx]+f(arr,idx+1,n);
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+    cout<<f(arr,0,n);
+
+    
+}
+ 
+
+
+
 
 
 
