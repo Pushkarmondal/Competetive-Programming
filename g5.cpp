ce1f469472cc7c83a340b62e6143f037a34a0eab
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2023-01-31 20:42:37 +0530

    Add files via upload

--- g5.cpp
+++ g5.cpp
@@ -66,14 +66,14 @@ using namespace std;
 #define INF 1e18
 #define endl "\n"
 #define pb push_back
-#define ppb pop_back;
+#define pb1 pop_back;
 #define vi vector<int>
 #define vintt vector<intt>
 #define vpi vector<pair<int,int>>
 #define vpl vector<pair<intt,intt>>
 #define getl(s) getline(cin,s)
-#define ff first
-#define ss second
+#define fi first
+#define se second
 #define PI 3.141592653589793238462
 #define bp __builtin_popcount
 #define sz(x) (x.size())
@@ -82,6 +82,7 @@ using namespace std;
 #define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
 #define fast ios::sync_with_stdio(0) , cin.tie(0) , cout.tie(0);
 //#define ld long double
+// ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
@@ -104,6 +105,7 @@ typedef long double lld;
 // vector<ll> PrimeFactorize(ll num){vector<ll>spf;spf=SieveOfEratosthenesExtended(num);set<ll>pf;while(num!=1){
 //         pf.insert(spf[num]);num=num/spf[num];}vector<ll>v;for(auto it:pf){v.pb(it);}return v;}
  
+ ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
  // ***********************************************************************************************************************************************************************************************************
 /*PUSHKAR MONDAL
 VELLORE INSTITUTE OF TECHNOLOGY*/
@@ -349,7 +351,7 @@ int min=*min_element(all(a));
 
 
 // }
- SUBSET PROBLEM USING RECURSION
+// SUBSET PROBLEM USING RECURSION
 
 // void func(int ind,vector<int>&ds,int arr[],int n){
 //     if(ind==n){
@@ -365,218 +367,75 @@ int min=*min_element(all(a));
 //     func(ind+1,ds,arr,n);
 
 //
+// 2D RECURSION RAT IN A MAZE STARTING***********
+// int n,m;
+// void findpath(int i,int j,string s){
+//     // BASE CASE FOR BOUNDARY.
+//     if(i>=n || i>=m)
+//         return;
 
+// // IF WE FIND ANSWER WE CAN RETURN ONE OF ANSWER.
+//     if(i==n-1 && j==m-1){
+//         cout<<s<<endl;
+//         return;
+//     }
+//     //MOVING DOWNSIDE.
+//     s+='D';
+//     findpath(i+1,j,s);
+//     s.pop_back();
 
-*****STRIVER RECURSION*****
+//     //MOVING RIGHTSIDE.
+//     s+='R';
+//     findpath(i,j+1,s);
+//     s.pop_back();
 
- (Q) 2D RECURSION RAT AND MOOSE STARTING***********
-
-int n,m;
-void findpath(int i,int j,string s){
-    // BASE CASE FOR BOUNDARY.
-    if(i>=n || i>=m)
-        return;
-
-// IF WE FIND ANSWER WE CAN RETURN ONE OF ANSWER.
-    if(i==n-1 && j==m-1){
-        cout<<s<<endl;
-        return;
-    }
-    //MOVING DOWNSIDE.
-    s+='D';
-    findpath(i+1,j,s);
-    s.pop_back();
-
-    //MOVING RIGHTSIDE.
-    s+='R';
-    findpath(i,j+1,s);
-    s.pop_back();
-
-} 
-
-*****STRIVER RECURSION*****
-
-(Q)>>
- SUM OF N NUMBERS>>>>
-
-#include<iostream>
-using namespace std;
-void f(int i,int sum){
-    if(i<1){
-        cout<<sum<<endl;
-        return;
-    }else{
-        f(i-1,sum+i);
-    }
-
-}
-int main(){
-   int i;
-   cin>>i;
-   f(i,0);
-
-}
-
-
- (*Q) REVERSE AN ARRAY USING RECURSION>>>>>>
-
-void f(int a[],int i,int n){
-    if(i>n/2)
-        return;
-    swap(a[i],a[n-i-1]);
-    f(a,i+1,n);
-    
-}
-int main(){
-    int n;
-    cin>>n;
-    int a[n];
-    for (int i = 0; i <n; ++i)
-    
-        cin>>a[i];
-    f(a,0,n);
-    for (int i = 0; i <n; ++i)
-    {
-        cout<<a[i]<<" ";
-    }
- 
- 
-
- (*Q)GIVEN STRING IS PALINDROME OR NOT >>>>
- 
- bool f(int i,string &s){
-    if(i>=s.size()/2)
-        return true;
-    if(s[i]!=s[s.size()-i-1])
-        return false;
-    return f(i+1,s);
-
-}
-  
-  
-int main(){
-    string s;
-    cin>>s;
-    cout<<f(0,s);
-}
-
-
-
-(*Q)MULTIPLE RECURSION CALL >>>>>>
-
-int f(int n){
-    if(n<=1) return n;
-    int last=f(n-1);
-    int slast=f(n-2);
-    return last+slast;
-}
-int main(){
-cout<<f(4);
-}
-
-
-(*Q)SUBSEQUENCE USING RESURSION >>>>>
-
-void printf(int ind,vector<int> &ds,int a[],int n){
-    if(ind==n){
-        for(auto it: ds){
-            cout<<it<<" ";
-        }
-        cout<<endl;
-        return;
-    }
-    ds.push_back(a[ind]);
-    printf(ind+1,ds,a,n);
-    ds.pop_back();
-    printf(ind+1,ds,a,n);
-}
-
-int main(){
-    int a[]={3,1,2};
-    int n=3;
-    vector<int> ds;
-    printf(0,ds,a,n);
-}
-
-(*Q)SUM OF 2 INDEX USING RECURSION >>>>>>
-
-void printf(int ind,vector<int> &ds,int s,int sum,int a[],int n){
-    if(ind==n){
-        if(s==sum){
-        for(auto it: ds)
-            cout<<it<<" ";
-            cout<<endl;
-        }
-        return;
-    }
-    ds.push_back(a[ind]);
-    s+=a[ind];
-    printf(ind+1,ds,s,sum,a,n);
-    s-=a[ind];
-    ds.pop_back();
-    printf(ind+1,ds,s,sum,a,n);
-}
-
-int main(){
-    int a[]={1,2,1};
-    int n=3;
-    int sum=2;
-    vector<int> ds;
-    printf(0,ds,0,sum,a,n);
-     
-}
-
-(*Q) FIND POWER OF GIVEN NUMBER USING RECURSION >>>>>>>
-
-int f(int p,int q){
-    if(q==0)
-        return 1;
-    return p*f(p,q-1);
-}
-int main(){
-  ll p,q;cin>>p>>q;
-  int result=f(p,q);
-  cout<<result<<endl; 
-}
-
-
-
-(*Q) SUM OF ALL ELEMENTS OF AN ARRAY >>>>>>
-
-int f(int *arr,int idx,int n){
-    if(idx==n-1){
-        return  arr[idx];
-    }
-    return arr[idx]+f(arr,idx+1,n);
-}
-int main(){
-    int arr[]={1,2,3,4,5};
-    int n=5;
-    cout<<f(arr,0,n);
-
-    
-}
- 
-
-
-
-
+// } 
+// int main(){
+//     cin>>n>>m;
+//     findpath(0,0,"");
+// }
 
 
 // const int m=1e9+7;
-//   void solve(){
-//     int a,b;
-//     cin>>a>>b;
-//     cout<<a+b<<endl;
+
+/* COUNT ALL NUMBER FROM 1 T0 N.
        
-// }
-    
- 
-// int main(){
-//      // solve();
+       ll n;
+    cin>>n;
+    ll ans=0;
+    for (ll i =1; i <=n; i*=10)
+    {
+        ans+=(n-i+1);
+    }
+*/
+
+
+
+
+
+int main(){
+    string s;
+    cin>>s;
+    int hash[256]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]]++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        cout<<hash[c]<<endl;
+
+    }
+
+} 
+  
+// int main(){   
 //     int t;
 //        cin>>t;
 //        while(t--){
 //         solve();
 //        }
+//     return 0;
 // }
